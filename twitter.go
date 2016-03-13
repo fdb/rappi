78645472f66fad4f6aad24a7f75f575bbb401aec
Frederik De Bleser <frederik@debleser.be>
2016-03-13 14:55:25 +0100

    Remove cruft from Twitter search results.

--- twitter.go
+++ twitter.go
@@ -4,9 +4,32 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/ChimeraCoder/anaconda"
 )
+
+type TwitterStatus struct {
+	Url           string   `json:"url"`
+	Id            int64    `json:"id"`
+	CreatedAt     string   `json:"createdAt"`
+	Text          string   `json:"text"`
+	UserId        int64    `json:"userId"`
+	UserHandle    string   `json:"user"`
+	FavoriteCount int      `json:"favoriteCount"`
+	RetweetCount  int      `json:"retweetCount"`
+	ImageUrls     []string `json:"imageUrls"`
+}
+
+type TwitterOk struct {
+	Status string          `json:"status"`
+	Tweets []TwitterStatus `json:"tweets"`
+}
+
+type TwitterError struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+}
 
 var twitterApi *anaconda.TwitterApi
 
@@ -16,16 +39,43 @@ func initTwitter(consumerKey, consumerSecret, accessToken, accessTokenSecret str
 	twitterApi = anaconda.NewTwitterApi(accessToken, accessTokenSecret)
 }
 
+func twitterStatusUrl(status anaconda.Tweet) string {
+	return fmt.Sprintf("https://twitter.com/%s/status/%s", status.User.ScreenName, status.IdStr)
+}
+
+func jsonError(w http.ResponseWriter, err error) {
+	errorString := fmt.Sprintf("%v", err)
+	fmt.Fprintf(w, `{"status":"error","message":%s}`, strconv.Quote(errorString))
+}
+
 func handleTwitterSearch(w http.ResponseWriter, r *http.Request) {
 	query := r.FormValue("q")
 	searchResult, err := twitterApi.GetSearch(query, nil)
 	if err != nil {
-		fmt.Fprintf(w, "Twitter API error: %v", err)
+		jsonError(w, err)
 		return
 	}
-	b, err := json.Marshal(searchResult.Statuses)
+	var statuses []TwitterStatus
+	for _, status := range searchResult.Statuses {
+		ts := TwitterStatus{
+			Url:           twitterStatusUrl(status),
+			Id:            status.Id,
+			CreatedAt:     status.CreatedAt,
+			Text:          status.Text,
+			UserId:        status.User.Id,
+			UserHandle:    status.User.ScreenName,
+			FavoriteCount: status.FavoriteCount,
+			RetweetCount:  status.RetweetCount,
+		}
+		ts.ImageUrls = make([]string, 0)
+		for _, media := range status.Entities.Media {
+			ts.ImageUrls = append(ts.ImageUrls, media.Media_url_https)
+		}
+		statuses = append(statuses, ts)
+	}
+	b, err := json.Marshal(TwitterOk{Status: "ok", Tweets: statuses})
 	if err != nil {
-		fmt.Fprintf(w, "Twitter JSON Error: %v", err)
+		jsonError(w, err)
 		return
 	}
 	fmt.Fprintf(w, "%s", b)
