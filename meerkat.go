2ba35d260c986b2f6a7e62422a8f4cbe3e987d7b
Frederik De Bleser <frederik@debleser.be>
2016-03-13 16:40:39 +0100

    Make Meerkat more robust.
    No more crashing if something goes wrong.

--- meerkat.go
+++ meerkat.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 )
 
@@ -44,12 +43,11 @@ func getBroadcasts() ([]Broadcast, error) {
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", "https://resources.meerkatapp.co/broadcasts", nil)
 	if err != nil {
-		log.Fatal("Could not create request object.")
+		return nil, err
 	}
 	req.Header.Add("Authorization", meerkatKey)
 	res, err := client.Do(req)
 	if err != nil {
-		log.Fatal("Could not perform request", err)
 		return nil, err
 	}
 	defer res.Body.Close()
@@ -57,7 +55,6 @@ func getBroadcasts() ([]Broadcast, error) {
 	var result Result
 	err = json.Unmarshal(body, &result)
 	if err != nil {
-		fmt.Println("error:", err)
 		return nil, err
 	}
 	return result.Result, nil
