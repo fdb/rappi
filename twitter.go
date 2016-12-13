b996cc0518455221efd64db9a216520c4c47d274
Frederik De Bleser <frederik@debleser.be>
2016-12-13 13:04:16 +0200

    Add caching.

--- twitter.go
+++ twitter.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-
+	"time"
 	"github.com/ChimeraCoder/anaconda"
 )
 
@@ -25,25 +25,30 @@ type TwitterOk struct {
 	Tweets []TwitterStatus `json:"tweets"`
 }
 
+type TwitterSearchResult struct {
+	Query string
+	Statuses []TwitterStatus
+	Time time.Time
+}
+
 var twitterApi *anaconda.TwitterApi
+var twitterCache map[string]*TwitterSearchResult
 
 func initTwitter(consumerKey, consumerSecret, accessToken, accessTokenSecret string) {
 	anaconda.SetConsumerKey(consumerKey)
 	anaconda.SetConsumerSecret(consumerSecret)
 	twitterApi = anaconda.NewTwitterApi(accessToken, accessTokenSecret)
+	twitterCache = make(map[string]*TwitterSearchResult)
 }
 
 func twitterStatusUrl(status anaconda.Tweet) string {
 	return fmt.Sprintf("https://twitter.com/%s/status/%s", status.User.ScreenName, status.IdStr)
 }
 
-func handleTwitterSearch(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	query := r.FormValue("q")
+func doTwitterSearch(query string) (*TwitterSearchResult, error) {
 	searchResult, err := twitterApi.GetSearch(query, nil)
 	if err != nil {
-		jsonError(w, err)
-		return
+		return nil, err
 	}
 	var statuses []TwitterStatus
 	for _, status := range searchResult.Statuses {
@@ -63,7 +68,27 @@ func handleTwitterSearch(w http.ResponseWriter, r *http.Request) {
 		}
 		statuses = append(statuses, ts)
 	}
-	b, err := json.Marshal(TwitterOk{Status: "ok", Tweets: statuses})
+
+	return &TwitterSearchResult{query, statuses, time.Now()}, nil
+}
+
+func handleTwitterSearch(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	query := r.FormValue("q")
+	result1, ok := twitterCache[query]
+	if (!ok || time.Now().Sub(result1.Time) > 5 * time.Minute) {
+		fmt.Println("New search " + query)
+		result2, err := doTwitterSearch(query)
+		if err != nil {
+			jsonError(w, err)
+			return
+		}
+		twitterCache[query] = result2
+		result1 = result2
+	}
+
+	// searchResult, err := twitterApi.GetSearch(query, nil)
+	b, err := json.Marshal(TwitterOk{Status: "ok", Tweets: result1.Statuses})
 	if err != nil {
 		jsonError(w, err)
 		return
